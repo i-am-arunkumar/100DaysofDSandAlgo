bc69a12072f72016d43dfee94e1c2102258fd93c
Balamohan <51916730+Flamesabber24@users.noreply.github.com>
2020-03-22 04:34:27 +0530

    Day 6

--- stk_ll.c
+++ stk_ll.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 struct Node
 {
@@ -12,12 +13,50 @@ typedef struct Node node;
 int isempty(node *first);
 int isfull(node *n);
 node *push(node *first, int x);
+node *pop(node *first, int *x);
+int peep(node *first, int pos);
+void display(node *first);
 
 int main(void)
 {
     node *top = NULL;
-    top = push(top, 5);
-    top = push(top, 2);
+    
+    int cont = 1;
+
+    while (cont)
+    {
+        puts("1. push\n2. pop\n3. peep\n4. display\nPress any other key to exit");
+
+        int choice, value, pos;
+        scanf("%d", &choice);
+
+        switch (choice)
+        {
+            case 1: 
+                printf("%s", "Enter the number to push: ");
+                scanf("%d", &value);
+                top = push(top, value);
+                break;
+
+            case 2:
+                top = pop(top, &value);
+                printf("Popped element from the stack: %d\n", value);
+                break;
+
+            case 3:
+                printf("%s", "Enter the position: ");
+                scanf("%d", &pos);
+                printf("Element at %d: %d\n", pos, peep(top, pos));
+                break;
+
+            case 4:
+                display(top);
+                break;
+
+            default:
+                cont = 0;
+        }
+    }
 
     return 0;
 }
@@ -52,4 +91,46 @@ node *push(node *first, int x)
         first = temp;
         return first;
     }    
+}
+
+node *pop(node *first, int *x)
+{
+    node *temp = first;
+    
+    if (isempty(first) == 1)
+    {
+        puts("The stack is empty.");
+        return NULL;
+    }
+
+    *x = first->data;
+    first = temp->next;
+    free(temp);
+
+    return first;
+}
+
+int peep(node *first, int pos)
+{
+    if (isempty(first) == 1)
+    {
+        puts("The stack is empty.");
+        return INT_MIN;
+    }
+
+    for (int i = 1; i < pos; i++)
+        first = first->next;
+
+    return first->data;
+}
+
+void display(node *first)
+{
+    while (first)
+    {
+        printf("%d ", first->data);
+        first = first->next;
+    }
+
+    puts("");
 }
