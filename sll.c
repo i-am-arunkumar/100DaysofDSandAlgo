cafe6030a6df5068c997b73af6fc5bef46f6fcf2
Balamohan <51916730+Flamesabber24@users.noreply.github.com>
2020-03-22 00:10:59 +0530

    Day 4-5

--- sll.c
+++ sll.c
@@ -9,61 +9,40 @@ struct Node
 
 typedef struct Node node;
 
-// node *create(int arr[], int size);
 void display(node *list);
 int count(node *list);
 node *insert(node *list, int pos, int element);
+node *insert_sorted(node *list, int element);
+node *delete_node(node *list, int pos, int *element);
 
 int main(void)
 {
-    // int A[] = {3, 2, 5, 9, 6};
-
-    // node *head = create(A, 5);
-
     node *head = (node *) malloc(sizeof(node));
     head = NULL;
     
-    head = insert(head, 0, 3);
+    head = insert(head, 0, 1);
     head = insert(head, 1, 2);
     head = insert(head, 2, 5);
-    head = insert(head, 3, 9);
-    head = insert(head, 4, 6);
+    head = insert(head, 3, 6);
+    head = insert(head, 4, 9);
 
     display(head);
 
     printf("Number of nodes in the list: %d\n", count(head));
 
-    insert(head, 3, 8);
+    head = insert_sorted(head, 4);
 
+    display(head);
+
+    int value = 0;
+    head = delete_node(head, 5, &value);
+    printf("The value at index 5 is: %d\n", value);
     display(head);        
 
     free(head);
 
     return 0;
 }
-
-/*
-node *create(int arr[], int size)
-{
-    node *first, *last, *temp;
-
-    first = (node *) malloc(sizeof(node));
-    first->data = arr[0];
-    first->next = NULL;
-    last = first;
-
-    for (int i = 1; i < size; i++)
-    {
-        temp = (node *) malloc(sizeof(node));
-        temp->data = arr[i];
-        temp->next = NULL;
-        last->next = temp;
-        last = temp;
-    }
-
-    return first;
-}
-*/
 
 void display(node *list)
 {
@@ -110,4 +89,64 @@ node *insert(node *list, int pos, int element)
     temp->next = first->next;
     first->next = temp;
     return list;
+}
+
+node *insert_sorted(node *list, int element)
+{
+    node *temp, *p = list, *q = NULL;
+
+    temp = (node *) malloc(sizeof(node));
+    temp->data = element;
+    temp->next = NULL;
+
+    if (list == NULL)
+    {
+        list = temp;
+        return list;
+    }
+    
+    while (p && p->data < element)
+    {
+        q = p;
+        p = p->next;
+    }
+
+    if (p == list)
+    {
+        temp->next = p;
+        list = temp;
+        return list;
+    }
+
+    temp->next = q->next;
+    q->next = temp;
+
+    return list;
+}
+
+node *delete_node(node *list, int pos, int *element)
+{
+    node *p = list, *q = NULL;
+
+    if (pos == 0)
+    {
+        list = p->next;
+        *element = p->data; 
+        free(p);
+        return list;
+    }
+
+    for (int i = -1; i < pos - 1 && p; i++)
+    {
+        q = p;
+        p = p->next;
+    }
+
+    if (p)
+    {
+        q->next = p->next;
+        *element = p->data;
+        free(p);
+        return list;
+    }
 }
